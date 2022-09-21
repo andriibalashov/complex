7782f7ad0cb167e93b419a6e25d902136358445c
andriibalashov <andriy.balashov@gmail.com>
2022-09-21 11:04:26 +0300

    deployment

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            ver1
+            ver 2
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
